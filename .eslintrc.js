8b92ab2569d8c7f8ae22e95409cbbb6f743038da
tronjsdev <tronjs.dev@gmail.com>
2020-01-03 10:20:56 +0700

    Add react hooks es lint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,24 +27,19 @@ module.exports = {
     sourceType: 'module',
     project: './tsconfig.eslint.json',
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
   rules: {
     //'@typescript-eslint/rule-name': 'error',
     'no-underscore-dangle': 'off',
     'no-debugger': 'off',
     'spaced-comment': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
     'import/extensions': 'off',
     'import/order': [
       'error',
       {
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          'parent',
-          'sibling',
-          'index',
-        ],
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
         'newlines-between': 'always',
       },
     ],
@@ -55,13 +50,10 @@ module.exports = {
     '@typescript-eslint/ban-ts-ignore': 'off',
     'import/prefer-default-export': 'off',
     'import/no-unresolved': 'off',
-    'react/jsx-filename-extension': [
-      1,
-      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
-    ],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
     'react/jsx-props-no-spreading': 'off',
     'react/prop-types': 'off',
     'jsx-a11y/anchor-is-valid': 'off',
-    'react/jsx-curly-brace-presence': 'off'
+    'react/jsx-curly-brace-presence': 'off',
   },
 };
